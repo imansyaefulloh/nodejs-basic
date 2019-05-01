70444ed702718318bc6364aae92aac1d92939381
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 18:34:11 +0700

    create empty file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
-fs.appendFile('mynewfile2.txt', 'Hello content 2!', (err) => {
+fs.open('mynewfile3.txt', 'w', (err, file) => {
   if (err) throw err;
   console.log('Saved');
 });

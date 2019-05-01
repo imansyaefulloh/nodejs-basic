61752ec21737c783183988d21444e25391ca6344
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 18:33:15 +0700

    create a file

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
-http.createServer((req, res) => {
-  fs.readFile('demofile1.html', (err, data) => {
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
-}).listen(3000);
+fs.appendFile('mynewfile2.txt', 'Hello content 2!', (err) => {
+  if (err) throw err;
+  console.log('Saved');
+});

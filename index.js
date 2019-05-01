80d42c44a62786ff33aa4c3648d5bc62bd191ba5
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 18:51:10 +0700

    using third party module using npm

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const uc = require('upper-case');
 
 http.createServer((req, res) => {
-  let q = url.parse(req.url, true);
-  let filename = "." + q.pathname;
-
-  console.log("pathname: " + q.pathname);
-  
-  fs.readFile(filename, (err, data) => {
-    if (err) {
-      res.writeHead(404, {'content-type': 'text/html'});
-      return res.end("404 Not Found");
-    }
-
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+  res.writeHead(200, {'content-type': 'text/html'});
+  res.write(uc('Hello World!'));
+  res.end();
 }).listen(3000);

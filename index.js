94c4f809c5a6e37f3da841d36d1e28c004f0a619
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 18:23:32 +0700

    basic modules

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
+const dt = require('./mymodule');
 
 http.createServer((req, res) => {
   res.writeHead(200, {'content-type': 'text/html'});
-  res.end('Hello World!');
+  res.end('The date and time currently: ' + dt.myDateTime());
 }).listen(3000);

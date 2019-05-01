f3043caaad3e018f1b33e5f92c9526b50f390a18
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 18:53:31 +0700

    basic event

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const http = require('http');
-const url = require('url');
 const fs = require('fs');
-const uc = require('upper-case');
+const rs = fs.createReadStream('./demofile1.txt');
 
-http.createServer((req, res) => {
-  res.writeHead(200, {'content-type': 'text/html'});
-  res.write(uc('Hello World!'));
-  res.end();
-}).listen(3000);
+rs.on('open', () => console.log('the file is open'));

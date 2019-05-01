7ac4b86a179d20b8d57aea786b725b9d860970f2
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 18:39:43 +0700

    rename file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
-fs.unlink('mynewfile3.txt', (err) => {
+fs.rename('mynewfile1.txt', 'mynewfile1_renamed.txt', (err) => {
   if (err) throw err;
   console.log('File deleted');
 });

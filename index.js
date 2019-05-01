95e7af8eb255884b8ea2ad598e31cb7583e13170
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 18:38:59 +0700

    deleting file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
-fs.writeFile('mynewfile3.txt', 'Old content are replaced by this text', (err, file) => {
+fs.unlink('mynewfile3.txt', (err) => {
   if (err) throw err;
-  console.log('Saved');
+  console.log('File deleted');
 });

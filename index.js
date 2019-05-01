fa43ce18b0b7dd310d7d143e77b9c048efc6f172
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 18:43:16 +0700

    basic built in url module

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const http = require('http');
 const url = require('url');
-const fs = require('fs');
 
-fs.rename('mynewfile1.txt', 'mynewfile1_renamed.txt', (err) => {
-  if (err) throw err;
-  console.log('File deleted');
-});
+let address = 'http://localhost:3000/default.htm?year=2019&month=february';
+let q = url.parse(address, true);
+
+console.log(q.host);
+console.log(q.pathname);
+console.log(q.search);
+
+let qdata = q.query;
+console.log(qdata.month);

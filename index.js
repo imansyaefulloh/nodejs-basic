89c6bba36fe7c3c6583381dbcaee7804327d8ef0
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 18:48:23 +0700

    serve html file

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
-let address = 'http://localhost:3000/default.htm?year=2019&month=february';
-let q = url.parse(address, true);
+http.createServer((req, res) => {
+  let q = url.parse(req.url, true);
+  let filename = "." + q.pathname;
 
-console.log(q.host);
-console.log(q.pathname);
-console.log(q.search);
+  console.log("pathname: " + q.pathname);
+  
+  fs.readFile(filename, (err, data) => {
+    if (err) {
+      res.writeHead(404, {'content-type': 'text/html'});
+      return res.end("404 Not Found");
+    }
 
-let qdata = q.query;
-console.log(qdata.month);
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(3000);

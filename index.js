0e5a50d08fe8abdd0f682f53d65a1fc00e3f4092
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 18:56:27 +0700

    event emitter

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const http = require('http');
 const fs = require('fs');
-const rs = fs.createReadStream('./demofile1.txt');
+const events = require('events');
+const eventEmitter = new events.EventEmitter();
 
-rs.on('open', () => console.log('the file is open'));
+// create an event handler
+let myEventHandler = () => console.log('I hear a scream');
+
+// assign the event handler to an event
+eventEmitter.on('scream', myEventHandler);
+
+// call the 'scream' event
+eventEmitter.emit('scream');

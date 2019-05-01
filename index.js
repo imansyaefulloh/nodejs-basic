c51cd1dd4473a55685c32e74dbf0cd461e5d5c8c
Iman Syaefulloh <imansyaefulloh@gmail.com>
2019-05-01 18:27:04 +0700

    split query string

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require('http');
-const dt = require('./mymodule');
+const url = require('url');
 
 http.createServer((req, res) => {
   res.writeHead(200, {'content-type': 'text/html'});
-  res.write(req.url);
-  res.end();
+  let q = url.parse(req.url, true).query;
+  let txt = q.year + " " + q.month;
+  res.end(txt);
 }).listen(3000);
